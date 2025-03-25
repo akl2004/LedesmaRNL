ce451ae8387e8bae560ad26cde8892c2f2b94078
akl2004 <theakimledesma@gmail.com>
2025-03-25 23:09:11 +0800

    loadingGenders

--- server/app/Http/Controllers/Api/GenderController.php
+++ server/app/Http/Controllers/Api/GenderController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Gender;
 use Illuminate\Http\Request;
 
 class GenderController extends Controller
 {
-    public function StoreGender(Request $request) {
+    public function loadGenders() {
+        $genders = Gender::all();
+        return response()->json([
+            'genders' => $genders
+        ], 200);
+    }
+
+    public function storeGender(Request $request) {
         $validated = $request->validate([
             'gender' => ['required', 'min:4', 'max:10']
         ]);
 
         Gender::create([
-            'gender' => $vallidated['gender']
+            'gender' => $validated['gender']
         ]);
 
         return response()->json([
